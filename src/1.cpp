1ccd17f781811071476be9e8223b9bc13ef19335
jayson9tsjeancy <jayson9tsjeancy@gmx.com>
2025-03-05 18:14:05 +0000

    Add 1.cpp

---
+++ src/1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    double x = 5;
+    double y = 2;
+    cout << "The value of x is: " << x << endl;
+    cout << "The value of y is: " << y << endl;
+    cout << "The sum of x and y is: " << x + y << endl;
+    return 0;
+}
